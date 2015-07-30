815bdfb00e364173aff8840f0eea4f6c21e9356f
EnquiringStone <johan-langes@hotmail.com>
2015-07-30 15:37:56 +0200

    Added helper classes to identify types of vehicles. Includes Blufor, Independent and Opfor vehicles (cars) and helicopters

---
+++ SpecialOperations2.Altis/sos/utils/SOS_VehicleTypes.hpp
@@ -0,0 +1,149 @@
+class SOS_VehicleTypes {
+	class TransportHelicopters {
+		vehicles[] = {
+			"B_Heli_Light_01_F",
+			"B_Heli_Transport_01_F",
+			"B_Heli_Transport_01_camo_F",
+			"B_Heli_Transport_03_F",
+			"B_Heli_Transport_03_unarmed_F",
+			"I_Heli_light_03_F",
+			"I_Heli_light_03_unarmed_F",
+			"I_Heli_Transport_02_F",
+			"O_Heli_Attack_02_F",
+			"O_Heli_Attack_02_black_F",
+			"O_Heli_Light_02_F",
+			"O_Heli_Light_02_unarmed_F",
+			"O_Heli_Light_02_v2_F",
+			"O_Heli_Transport_04_bench_F",
+			"O_Heli_Transport_04_covered_F"
+		};
+	};
+	
+	class TransportVehicles {
+		vehicles[] = {
+			"B_G_Offroad_01_F",
+			"B_G_Van_01_transport_F",
+			"B_MRAP_01_F",
+			"B_MRAP_01_gmg_F",
+			"B_MRAP_01_hmg_F",
+			"B_Truck_01_covered_F",
+			"B_Truck_01_transport_F",
+			"B_Quadbike_01_F",
+			"I_MRAP_03_F",
+			"I_MRAP_03_gmg_F",
+			"I_MRAP_03_hmg_F",
+			"I_Truck_02_covered_F",
+			"I_Truck_02_transport_F",
+			"O_MRAP_02_F",
+			"O_MRAP_02_hmg_F",
+			"O_MRAP_02_gmg_F",
+			"O_Truck_02_covered_F",
+			"O_Truck_02_transport_F"
+		};
+	};
+	
+	class ArmedHelicopters {
+		vehicles[] = {
+			"B_Heli_Attack_01_F",
+			"B_Heli_Light_01_armed_F",
+			"B_Heli_Transport_01_F",
+			"B_Heli_Transport_01_camo_F",
+			"B_Heli_Transport_03_F",
+			"I_Heli_light_03_F",
+			"O_Heli_Attack_02_F",
+			"O_Heli_Attack_02_black_F",
+			"O_Heli_Light_02_F"
+		};
+	};
+	
+	class ArmedVehicles {
+		vehicles[] = {
+			"B_G_Offroad_01_armed_F",
+			"B_MRAP_01_gmg_F",
+			"B_MRAP_01_hmg_F",
+			"I_MRAP_03_gmg_F",
+			"I_MRAP_03_hmg_F",
+			"O_MRAP_02_gmg_F",
+			"O_MRAP_02_hmg_F"
+		};
+	};
+	
+	class LogisticsHelicopters {
+		vehicles[] = {
+			"B_Heli_Transport_01_F",
+			"B_Heli_Transport_01_camo_F",
+			"O_Heli_Transport_04_ammo_F",
+			"O_Heli_Transport_04_box_F",
+			"O_Heli_Transport_04_fuel_F",
+			"O_Heli_Transport_04_medevac_F",
+			"O_Heli_Transport_04_repair_F"
+		};
+	};
+	
+	class LogisticsVehicles {
+		vehicles[] = {
+			"B_G_Offroad_01_repair_F",
+			"B_G_Van_01_fuel_F",
+			"B_Truck_01_ammo_F",
+			"B_Truck_01_box_F",
+			"B_Truck_01_fuel_F",
+			"B_Truck_01_medical_F",
+			"B_Truck_01_Repair_F",
+			"I_Truck_02_ammo_F",
+			"I_Truck_02_box_F",
+			"I_Truck_02_fuel_F",
+			"I_Truck_02_medical_F",
+			"O_Truck_02_Ammo_F",
+			"O_Truck_02_box_F",
+			"O_Truck_02_fuel_F",
+			"O_Truck_02_medical_F",
+			"O_Truck_03_ammo_F",
+			"O_Truck_03_fuel_F",
+			"O_Truck_03_medical_F",
+			"O_Truck_03_repair_F"
+		};
+	};
+	
+	class Medical {
+		vehicles[] = {
+			"B_Truck_01_medical_F",
+			"I_Truck_02_medical_F",
+			"O_Heli_Transport_04_medevac_F",
+			"O_Truck_02_medical_F",
+			"O_Truck_03_medical_F"
+		};
+	};
+	
+	class Repair {
+		vehicles[] = {
+			"B_G_Offroad_01_repair_F",
+			"B_Truck_01_Repair_F",
+			"I_Truck_02_box_F",
+			"O_Heli_Transport_04_repair_F",
+			"O_Truck_02_box_F",
+			"O_Truck_03_repair_F"
+		};
+	};
+	
+	class Fuel {
+		vehicles[] = {
+			"B_G_Van_01_fuel_F",
+			"B_Truck_01_fuel_F",
+			"I_Truck_02_fuel_F",
+			"O_Heli_Transport_04_fuel_F",
+			"O_Truck_02_fuel_F",
+			"O_Truck_03_fuel_F"
+		};
+	};
+	
+	class Ammo {
+		vehicles[] = {
+			"B_Truck_01_ammo_F",
+			"B_Truck_01_box_F",
+			"I_Truck_02_ammo_F",
+			"O_Heli_Transport_04_box_F",
+			"O_Truck_02_Ammo_F",
+			"O_Truck_03_ammo_F"
+		};
+	};
+};
