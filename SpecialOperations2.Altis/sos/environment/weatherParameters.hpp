4cb4f440a317859e7a4bc60dc73718864190a96c
Catshannon <the_catshannon@hotmail.com>
2015-08-05 20:40:12 +0100

    Update weatherParameters.hpp

--- SpecialOperations2.Altis/sos/environment/weatherParameters.hpp
+++ SpecialOperations2.Altis/sos/environment/weatherParameters.hpp
@@ -1,24 +1,24 @@
 class Overcast {
     title	= "Weather: Overcast (100% means storms and rain are very likely)";
-    values[]   = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    values[]    = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
     texts[]	= {"0%", "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"};
     default	= 0;
 };
 class Fog {
     title	= "Weather: Fog (Past 50% is unbearable at some altitudes)";
-    values[]   = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    values[]    = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
     texts[]	= {"0%", "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"};
     default	= 0;
 };
 class Rain {
     title	= "Weather: Rain (70% is required for rain)";
-    values[]   = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    values[]    = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
     texts[]	= {"0%", "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"};
     default	= 0;
 };
 class RandomWeather {
     title	= "Weather: Random (Will override above settings by default)";
-    values[]   = {1, 0};
+    values[]    = {1, 0};
     texts[]	= {"True (Default)", "False"};
     default	= 1;
 };
