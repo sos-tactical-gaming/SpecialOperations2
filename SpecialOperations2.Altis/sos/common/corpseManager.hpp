7bb0c9ebcb1e2184089b9b37b99692a43c25f937
Catshannon <the_catshannon@hotmail.com>
2015-08-07 15:43:14 +0100

    Corpse Manager
    Adds a .hpp with values for removing bodies and wrecks.

---
+++ SpecialOperations2.Altis/sos/common/corpseManager.hpp
@@ -0,0 +1,8 @@
+corpseManagerMode = 1; //All dead
+corpseLimit = 24;
+corpseRemovalMinTime = 300; //seconds
+corpseRemovalMaxTime = 1200; //seconds
+wreckManagerMode = 1; //All dead
+wreckLimit = 5;
+wreckRemovalMinTime = 300; //seconds
+wreckRemovalMaxTime = 1200; //seconds
